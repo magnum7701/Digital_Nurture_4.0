f8d1d7ded33f585fb724b4bf6bef6c2d5a0e37c0
Soham Ghosh <ghoshsoham1708@gmail.com>
2025-07-14 01:15:55 +0530

    Added 4 assignments of Spring Rest using Spring  Boot3

---
+++ 4_SpringREST_using_SpringBoot/1_CreateSpringWebProjectUsingMaven/Code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,21 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting SpringLearnApplication...");
+
+		SpringApplication.run(SpringLearnApplication.class, args);
+
+		logger.info("SpringLearnApplication started successfully.");
+	}
+}
